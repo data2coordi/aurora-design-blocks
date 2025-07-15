2df8c9594b977cb5a2b7af739e03e2fd2e732906
data2coordi <data2coordi@gmail.com>
2025-07-16 05:57:28 +0900

    test

--- plugin.php
+++ plugin.php
@@ -39,7 +39,7 @@ require AURORA_DESIGN_BLOCKS_PATH . '/inc/aurora-design-blocks-awesome.php';
 
 
 ///////////////////////////////////////////
-//プラグインの自動更新s//////abc/
+//プラグインの自動更新s//////abcd/
 ///////////////////////////////////////////
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
