853b2aa6a8bf0bd58d55db35fd088e90bef2004a
data2coordi <data2coordi@gmail.com>
2025-07-16 08:22:55 +0900

    バージョン更新のテスト

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Aurora Design Blocks 
  * Description: カスタムブロックを追加するプラグイン。
- * Version: 1.0.1
+ * Version: 1.0.3
  * Author: Yurika Toshida at Aurora Lab
  * Text Domain: aurora-design-blocks
  */
