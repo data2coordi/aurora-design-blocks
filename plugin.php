5cfe1d2015ee52107f3275b756d418444525f99d
data2coordi <data2coordi@gmail.com>
2025-07-15 11:51:19 +0900

    更新通知デバッグ用

--- plugin.php
+++ plugin.php
@@ -6,45 +6,13 @@
  * Version: 1.0.3
  * Author: Yurika Toshida at Aurora Lab
  * Text Domain: aurora-design-blocks
- * GitHub Plugin URI: https://github.com/data2coordi/aurora-design-blocks
- * GitHub Release Asset: true
- * Primary Branch: master
+ * Update URI: https://integlight.auroralab-design.com/wp-content/updates/aurora-design-blocks.json
  */
-
-// PSR-4 オートローダー
-spl_autoload_register(function ($class) {
-    $prefix = 'Fragen\\Git_Updater\\';
-    $base_dir = __DIR__ . '/third-party/Git_Updater/';
-
-    if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, strlen($prefix));
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file)) {
-        require $file;
-    }
-});
-
-// Bootstrapクラスを読み込み（オートローダーで読めるため require は任意）
-require_once __DIR__ . '/third-party/Git_Updater/Bootstrap.php';
-
-// プラグイン初期化
-add_action('plugins_loaded', function () {
-    if (class_exists('\Fragen\Git_Updater\Bootstrap')) {
-        new \Fragen\Git_Updater\Bootstrap();
-    }
-});
-
-//var_dump(__DIR__ . '/third-party/Git_Updater/Bootstrap.php');
 
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
     define('_S_VERSION', '1.0.1');
 }
-
 
 
 if (!defined('ABSPATH')) {
