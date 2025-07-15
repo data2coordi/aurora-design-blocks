786ef1e1121f875d1dd74a7da827a97e1596d87e
data2coordi <data2coordi@gmail.com>
2025-07-15 09:06:07 +0900

    自動更新導入

--- plugin.php
+++ plugin.php
@@ -6,7 +6,39 @@
  * Version: 1.0.1
  * Author: Yurika Toshida at Aurora Lab
  * Text Domain: aurora-design-blocks
+ * GitHub Plugin URI: https://github.com/data2coordi/aurora-design-blocks
+ * GitHub Release Asset: true
+ * Primary Branch: master
  */
+
+// PSR-4 オートローダー
+spl_autoload_register(function ($class) {
+    $prefix = 'Fragen\\Git_Updater\\';
+    $base_dir = __DIR__ . '/third-party/Git_Updater/';
+
+    if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, strlen($prefix));
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+// Bootstrapクラスを読み込み（オートローダーで読めるため require は任意）
+require_once __DIR__ . '/third-party/Git_Updater/Bootstrap.php';
+
+// プラグイン初期化
+add_action('plugins_loaded', function () {
+    if (class_exists('\Fragen\Git_Updater\Bootstrap')) {
+        new \Fragen\Git_Updater\Bootstrap();
+    }
+});
+
+//var_dump(__DIR__ . '/third-party/Git_Updater/Bootstrap.php');
 
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
