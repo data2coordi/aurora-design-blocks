bff10cbcfd7bbc2a38e43668cdcfdb3c12ce071e
data2coordi <data2coordi@gmail.com>
2025-07-15 21:20:18 +0900

    自動更新・ダウンロード導入

--- plugin.php
+++ plugin.php
@@ -35,3 +35,23 @@ require AURORA_DESIGN_BLOCKS_PATH . '/inc/aurora-design-blocks-forBlocks.php';
 require AURORA_DESIGN_BLOCKS_PATH . '/inc/aurora-design-blocks-customizer.php';
 require AURORA_DESIGN_BLOCKS_PATH . '/inc/aurora-design-blocks.php';
 require AURORA_DESIGN_BLOCKS_PATH . '/inc/aurora-design-blocks-awesome.php';
+
+
+
+///////////////////////////////////////////
+//プラグインの自動更新s//
+///////////////////////////////////////////
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+// ライブラリの読み込み（相対パスは環境に合わせて修正）
+require_once __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+
+// アップデートチェッカーのインスタンス作成
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+    'https://integlight.auroralab-design.com/wp-content/updates/aurora-design-blocks.json', // WebアクセスできるJSONのURL
+    __FILE__,   // プラグインのメインファイルの絶対パス（ここでは現在のファイル）
+    'aurora-design-blocks' // プラグインの一意のスラッグ（通常はフォルダ名）
+);
+///////////////////////////////////////////
+//プラグインの自動更新e//
+///////////////////////////////////////////
