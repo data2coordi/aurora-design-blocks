332b660d013c1bd8b373ee50a0eb7c5e47c007de
data2coordi <data2coordi@gmail.com>
2025-07-27 16:24:37 +0900

    v1.0.12

--- aurora-design-blocks.php
+++ aurora-design-blocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Aurora Design Blocks 
  * Description: カスタムブロックを追加するプラグイン。
- * Version: 1.0.11
+ * Version: 1.0.12
  * Author: Yurika Toshida at Aurora Lab
  * Text Domain: aurora-design-blocks
  */
