20fc4fd3ed9b587c19e1f67c89a4cf9be9a23e42
data2coordi <data2coordi@gmail.com>
2025-09-05 18:03:50 +0900

    mo,json作成スクリプト導入

---
+++ util/chPoToMo.sh
@@ -0,0 +1,17 @@
+######## ja.poに英語に対応する日本語を記載する。(CHATGpt活用可能）aurora-design-blocks s
+## delete
+rm -f ../languages/*.mo
+rm -f ../languages/*.json
+
+## mo生成
+src=../languages/ja.po
+tgt=../languages/aurora-design-blocks-ja.mo
+msgfmt ${src} -o ${tgt}
+
+## javascript用json生成生成
+src=../languages
+wp i18n  make-json ${src} --allow-root --no-purge
+#exit
+
+
+######## ja.poに英語に対応する日本語を記載する。(CHATGpt活用可能）aurora-design-blocks e
