77bf186f5c8bec97003f3648c573b88bb00b928b
data2coordi <data2coordi@gmail.com>
2025-09-07 19:46:50 +0900

    ブロックアイテムのCSS外だし対応。ctaのみ

--- blocks/cta-block/build/index.asset.php
+++ blocks/cta-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e979ae4b379c14c013ba');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '15c966b789ad48fd7129');
--- inc/aurora-design-blocks-forBlocks.php
+++ inc/aurora-design-blocks-forBlocks.php
@@ -101,3 +101,72 @@ add_action('enqueue_block_editor_assets', 'auroraDesignBlocks_enqueue_block_asse
 
 
 
+
+class AuroraDesignBlocksPreDetermineCssBlocks
+{
+	/**
+	 * ブロックごとのフロントCSS設定
+	 * 'block-name' => ['handle' => 'css-path']
+	 */
+	private static $blocks = [
+		'aurora-design-blocks/cta-block' => [
+			'aurora-design-blocks-cta-block-style' => 'blocks/cta-block/build/style-index.css',
+		],
+		'aurora-design-blocks/another-block' => [
+			'aurora-design-blocks-another-block-style' => 'blocks/another-block/build/style-index.css',
+		],
+		// 他ブロックもここに追加可能
+	];
+
+	private static $EditorStyles = [];
+	private static $deferredStyles = [];
+
+	/**
+	 * フロント用初期化
+	 */
+	public static function init()
+	{
+		// まず全ブロックを登録
+		foreach (self::$blocks as $blockName => $styles) {
+			foreach ($styles as $handle => $path) {
+				wp_register_style(
+					$handle,
+					AURORA_DESIGN_BLOCKS_URL . $path,
+					[],
+					'1.0.0'
+				);
+			}
+		}
+
+		// 各ブロックごとに判定してフロントでenqueue
+		foreach (self::$blocks as $blockName => $styles) {
+			if (is_singular() && has_block($blockName)) {
+				// フロント用に登録（enqueueではなく add_styles に登録）
+				AuroraDesignBlocksFrontendStyles::add_styles($styles);
+
+				// 遅延対象に追加
+				self::$deferredStyles = array_merge(self::$deferredStyles, array_keys($styles));
+			}
+		}
+
+		if (!empty(self::$deferredStyles)) {
+			AuroraDesignBlocksDeferCss::add_deferred_styles(self::$deferredStyles);
+		}
+	}
+
+	/**
+	 * エディター用初期化（全ブロック常時読み込む）
+	 */
+	public static function init_forEditor()
+	{
+		foreach (self::$blocks as $blockName => $styles) {
+			self::$EditorStyles = array_merge(self::$EditorStyles, $styles);
+		}
+
+		AuroraDesignBlocksEditorStyles::add_styles(self::$EditorStyles);
+	}
+}
+
+// 初期化フック
+add_action('wp', ['AuroraDesignBlocksPreDetermineCssBlocks', 'init']);
+add_action('init', ['AuroraDesignBlocksPreDetermineCssBlocks', 'init_forEditor']);
