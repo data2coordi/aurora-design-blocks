1ff9256d3164a0bc78c05a3ef8c6e20b50445167
data2coordi <data2coordi@gmail.com>
2025-09-27 20:27:38 +0900

    awesome subset

--- util/creFontList.sh
+++ util/creFontList.sh
@@ -1,3 +1,21 @@
+
+pyftsubset ../css/webfonts/fa-solid-900.woff2 \
+  --unicodes-file=awesomeFontList.txt \
+  --flavor=woff2 \
+  --output-file=fa-solid-900-subset.woff2
+
+
+
+exit
+
 cssFileName=../css/src/awesome-all.css
 
 grep -o '\\f[0-9a-f]\{3,4\}' ${cssFileName} | sort -u
+
+
+exit
+
+pyftsubset ../css/webfonts/fa-solid-900.woff2 \
+  --unicodes-file=awesomeFontList.txt \
+  --flavor=woff2 \
+  --output-file=fa-solid-900-subset.woff2
