9fe0dd111cd0d73ea01a738b2fd8c1eb68c05ed4
data2coordi <data2coordi@gmail.com>
2025-11-19 14:51:02 +0900

    freeの作り方を変更

--- util/git_free.sh
+++ util/git_free.sh
@@ -1,5 +1,20 @@
 clear
+
 git checkout master
+
+git push origin --delete free
+
+# 3. ローカルの dev ブランチを強制削除
+# -d は未マージを検知して停止しますが、-D は強制的に削除します
+git branch -D free
+
+# 4. 次の作業のための dev ブランチを再作成
+# 最新の jmaster を基に新しい dev を作成します
+git checkout -b free
+
+git checkout master
+
+
 git commit --allow-empty -m "free"
 git push  --force
 
