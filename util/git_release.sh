c60b34311960b34eb07c11002f2722b38873ece4
data2coordi <data2coordi@gmail.com>
2025-12-02 14:01:19 +0900

    リリースバッチ追加

---
+++ util/git_release.sh
@@ -0,0 +1,15 @@
+
+clear
+
+#exit
+
+#git checkout master
+
+ver=$(grep -i "^[[:space:]]*\*[[:space:]]*Version:" ../aurora-design-blocks.php | awk '{print $NF}')
+sed -i "s/define('AURORA_DESIGN_BLOCKS_VERSION', '[0-9.]*');/define('AURORA_DESIGN_BLOCKS_VERSION', '$ver');/" ../aurora-design-blocks.php
+sed -i "s/^Stable tag: [0-9.]*/Stable tag: $ver/" ../readme.txt
+
+git add ../aurora-design-blocks.php
+git add ../readme.txt
+
+git commit -m "v$ver-release  $(date '+%Y-%m-%d %H:%M:%S') douki" --allow-empty
